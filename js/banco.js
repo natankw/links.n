6af7abaf932320ad4bb108dd3d5a8c32ab03b6fe
natankw <hll931200@gmail.com>
2026-07-19 14:51:11 -0300

    Update banco.js

--- js/banco.js
+++ js/banco.js
@@ -1,54 +1,116 @@
 const SENHA_ADMIN = "10092008";
 
+
 let downloads = JSON.parse(localStorage.getItem("downloads")) || [];
 
 
+
 function salvarBanco(){
+
     localStorage.setItem(
         "downloads",
         JSON.stringify(downloads)
     );
+
 }
+
 
 
 function listarDownloads(){
+
     return downloads;
+
 }
+
 
 
 function buscarDownload(id){
-    return downloads.find(item => item.id == id);
+
+    return downloads.find(
+        item => item.id == id
+    );
+
 }
+
 
 
 function adicionarDownload(dados){
 
+
     dados.id = Date.now();
-    dados.data = new Date().toLocaleDateString("pt-BR");
+
+    dados.data = new Date()
+    .toLocaleDateString("pt-BR");
+
+
+    dados.views = 0;
+
+    dados.destaque = dados.destaque || false;
+
 
     downloads.push(dados);
 
+
     salvarBanco();
+
 }
+
+
+
 
 
 function editarDownload(id, novosDados){
 
-    let item = buscarDownload(id);
+
+    const item = buscarDownload(id);
+
 
     if(item){
+
         Object.assign(item, novosDados);
+
         salvarBanco();
+
     }
+
 
 }
 
 
+
+
 function excluirDownload(id){
 
+
     downloads = downloads.filter(
+
         item => item.id != id
+
     );
 
+
     salvarBanco();
+
+
+}
+
+
+
+
+
+function aumentarViews(id){
+
+
+    const item = buscarDownload(id);
+
+
+    if(item){
+
+        item.views++;
+
+        salvarBanco();
+
+    }
+
+
 }
