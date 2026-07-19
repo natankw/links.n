38c125c3248faa63f6e3dc188f5bdb66cc15f580
natankw <hll931200@gmail.com>
2026-07-19 15:19:17 -0300

    Create banco.js

---
+++ js/banco.js
@@ -0,0 +1,67 @@
+const SENHA_ADMIN = "10092008";
+
+let downloads = JSON.parse(localStorage.getItem("downloads")) || [];
+
+
+function salvarBanco(){
+    localStorage.setItem("downloads", JSON.stringify(downloads));
+}
+
+
+function listarDownloads(){
+    return downloads;
+}
+
+
+function buscarDownload(id){
+    return downloads.find(item => item.id == id);
+}
+
+
+function adicionarDownload(dados){
+
+    dados.id = Date.now();
+    dados.data = new Date().toLocaleDateString("pt-BR");
+    dados.views = 0;
+
+    downloads.push(dados);
+
+    salvarBanco();
+}
+
+
+function editarDownload(id,dados){
+
+    const item = buscarDownload(id);
+
+    if(item){
+
+        Object.assign(item,dados);
+
+        salvarBanco();
+
+    }
+}
+
+
+function excluirDownload(id){
+
+    downloads = downloads.filter(item => item.id != id);
+
+    salvarBanco();
+
+}
+
+
+function aumentarViews(id){
+
+    const item = buscarDownload(id);
+
+    if(item){
+
+        item.views++;
+
+        salvarBanco();
+
+    }
+}
